0bbb488fb9fb07f7130be3df546b107d995cba00
oyesaurav <sauravpati0407+apps@gmail.com>
2021-02-07 17:04:38 +0530

    added mycode.c

---
+++ mycode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+    //Sharing my today's experience
+    int n;
+    printf("Enter the session no. of psoc after explorer sessions : ");
+    scanf("%d",&n);
+    if(n==1)
+         printf("This session was on 'open source and git' and really a great session for such a beginner like  me.\n");
+    else
+         printf("This session is yet to be held \n");  
+}

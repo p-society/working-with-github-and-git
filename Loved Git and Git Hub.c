cd650846fbe7c88a3c52da69a12c81260983a59a
Zad-100 <78676673+Zad-100@users.noreply.github.com>
2021-02-07 15:55:23 +0530

    My gratitude towards Seniors
    This is a C file that prints four lines through which I am sharing my experience of the whole session. #1
    And showing my gratitude towards all the seniors behind this work . #2

---
+++ Loved Git and Git Hub.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%s", "\nHello, I am Akash Parida from IT branch and with ID 4.");
+    printf("%s", "\nAnd I really loved learning it. Still a long way away but at least I am started!");
+    printf("%s", "\nThank you, all of the absolutely amazing seniors for teaching us such ming boggling topic with such effort.");
+    printf("%s", "\nAlso, thank you for clearing all my doubts however dumb they were!\n");
+ }

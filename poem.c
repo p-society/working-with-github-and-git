98e34b4d46c72e1c109cc9598f1d81a55c8efe29
sanjyotid <strsanjyotidas@gmail.com>
2021-02-10 21:24:51 +0530

    added a poem.c file

---
+++ poem.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("DON'T QUIT\n");
+   
+ printf("When things go wrong, as they sometimes will,\n");
+ printf("When the road you're trudging seems all uphill,\n");
+ printf("When the funds are low and the debts are high,\n");
+ printf("And you want to smile, but you have to sigh,\n");
+ printf("When care is pressing you down a bit-\n");
+ printf("Rest if you must, but don't you quit.\n");
+ printf("Life is queer with its twists and turns,\n");
+
+
+ printf("The silver tint in the clouds of doubt,\n");
+ printf("And you never can tell how close you are,\n");
+ printf("It might be near when it seems afar;\n");
+ printf("So stick to the fight when you're hardest hit -\n");
+ printf("It's when things seem worst that you must not quit.\n");
+
+printf("BY JOHN GREENLEAF WHITTIER");
+
+return 0;
+}
